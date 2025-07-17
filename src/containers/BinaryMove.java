16d0c3b91b11694147c22bfc69f94e273bade284
TruthDefined <58312225+ TruthDefined@users.noreply.github.com>
2025-07-17 17:17:34 -0400

    Fixed BinaryMove storage and init functions

--- src/containers/BinaryMove.java
+++ src/containers/BinaryMove.java
@@ -1,29 +1,30 @@
 package containers;
 
+import constants.Constants;
 import utils.ByteUtils;
 import utils.ByteUtils.Index;
 
 public class BinaryMove {
     
-    private byte[] Energy;
-    private byte[] Name;
-    private byte[] Description;
-    private byte Damage;
-    private byte Catagory;
-    private byte[] Effect_cmds;
-    private byte Flags1;
-    private byte Flags2;
-    private byte Flags3;
-    private byte Unknown;
-    private byte End;
+    private byte[] Energy;      //02 00 00 00
+    private byte[] Name;        //0b 08
+    private byte[] Description; //0c 08 00 00
+    private byte Damage;        //14
+    private byte Category;      //00
+    private byte[] Effect_cmds; //11 48
+    private byte Flags1;        //00
+    private byte Flags2;        //02
+    private byte Flags3;        //00
+    private byte Unknown;       //01
+    private byte End;         //59???
+    //02 00 00 00 0b 08 0c 08 00 00 14 00 11 48 00 02 00 01 59
     
-    
-    public BinaryMove(byte[] Energy, byte[] Name, byte[] Description, byte Damage, byte Catagory, byte[] Effect_cmds, byte Flags1, byte Flags2, byte Flags3, byte Unknown, byte End){
+    public BinaryMove(byte[] Energy, byte[] Name, byte[] Description, byte Damage, byte Category, byte[] Effect_cmds, byte Flags1, byte Flags2, byte Flags3, byte Unknown, byte End){
         this.Energy = Energy;
         this.Name = Name;
         this.Description = Description;
         this.Damage = Damage;
-        this.Catagory = Catagory;
+        this.Category = Category;
         this.Effect_cmds = Effect_cmds;
         this.Flags1 = Flags1;
         this.Flags2 = Flags2;
@@ -33,26 +34,25 @@ public class BinaryMove {
 
     }
     
-    public BinaryMove(byte[] InputByteArray){
-        if (InputByteArray.length != 20) {
-                throw new IllegalArgumentException("BinaryMove requires exactly 20 bytes of data.");
-            }
+    public BinaryMove(byte[] InputByteArray) {
+        if (InputByteArray.length != Constants.PKMN_MOVE_DATA_LENGTH) {
+            throw new IllegalArgumentException("BinaryMove requires exactly 19 bytes of data.");
+        }
         Index index = new Index(0);
+
+        this.Energy = ByteUtils.readBytes(InputByteArray, index, 4);
+        this.Name = ByteUtils.readBytes(InputByteArray, index, 2);
+        this.Description = ByteUtils.readBytes(InputByteArray, index, 4);
         
-        this.Energy = (byte[]) ByteUtils.readBytes(InputByteArray, index, 4);
-        this.Name = (byte[]) ByteUtils.readBytes(InputByteArray, index, 2);
-        this.Description = (byte[]) ByteUtils.readBytes(InputByteArray, index, 4);
-        this.Damage = (byte) ByteUtils.readBytes(InputByteArray, index, 1);
-        this.Catagory = (byte) ByteUtils.readBytes(InputByteArray, index, 1);
-        this.Effect_cmds = (byte[]) ByteUtils.readBytes(InputByteArray, index, 2);
-        this.Flags1 = (byte) ByteUtils.readBytes(InputByteArray, index, 1);
-        this.Flags2 = (byte) ByteUtils.readBytes(InputByteArray, index, 1);
-        this.Flags3 = (byte) ByteUtils.readBytes(InputByteArray, index, 1);
-        this.Unknown = (byte) ByteUtils.readBytes(InputByteArray, index, 1);
-        this.End = (byte) ByteUtils.readBytes(InputByteArray, index, 1);
-    
-    
-    
+        // For single bytes, extract the first element of the returned array:
+        this.Damage = ByteUtils.readBytes(InputByteArray, index, 1)[0];
+        this.Category = ByteUtils.readBytes(InputByteArray, index, 1)[0];
+        this.Effect_cmds = ByteUtils.readBytes(InputByteArray, index, 2);
+        this.Flags1 = ByteUtils.readBytes(InputByteArray, index, 1)[0];
+        this.Flags2 = ByteUtils.readBytes(InputByteArray, index, 1)[0];
+        this.Flags3 = ByteUtils.readBytes(InputByteArray, index, 1)[0];
+        this.Unknown = ByteUtils.readBytes(InputByteArray, index, 1)[0];
+        this.End = ByteUtils.readBytes(InputByteArray, index, 1)[0];
     }
 }
 
